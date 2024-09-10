2ecafc41479ec4c512000885cd4304b0139192e3
Ashish Kumar <akdev5520@gmail.com>
2024-09-10 20:25:02 +0530

    sending summary to frontend

--- backend/api/processVideo.js
+++ backend/api/processVideo.js
@@ -1,9 +1,35 @@
 import generateSummary from "../services/summarizer.js";
 import downloadAudio from "../services/videoDownloader.js";
 
-const processVideo = () => {
+const processVideo = async (req, res) => {
 	console.log("processing the video...");
-    
+
+	try {
+		const { url } = req.body;
+		const outputPath = "./temp/output.mp3";
+
+		await downloadAudio(url, outputPath);
+		console.log("video downloaded successfully!");
+
+		const text = await generateSummary(
+			outputPath,
+			"Generate the transcript of this speech."
+		);
+		console.log("Summary generated successfully!");
+		console.log("summary: ", text);
+
+		return res.status(200).json({
+			status: 200,
+			message: "Video processed successfully!",
+			text,
+		});
+	} catch (err) {
+		console.error("Error processing video: ", err.message);
+		return res.status(500).json({
+			status: 401,
+			error: "Something went wrong!",
+		});
+	}
 };
 
 export default processVideo;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,57 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+	const [url, setUrl] = useState("");
+	const [text, setText] = useState("");
+
+	const handleSubmit = async (evt) => {
+		evt.preventDefault();
+		try {
+			// const response = await fetch(
+			// 	"http://localhost:5000/api/summarize",
+			// 	{
+			// 		method: "POST",
+			// 		headers: {
+			// 			"Content-Type": "application/json",
+			// 		},
+			// 		body: JSON.stringify({ url }),
+			// 	}
+			// );
+
+			const response = await axios.post(
+				"http://localhost:5000/api/summarize",
+				{ url }
+			);
+
+			if (response.status === 200) {
+				console.log("Data successfully submitted!");
+				console.log("response = ", response);
+				setText(response.data.text);
+			} else {
+				console.error("Error submitting data");
+			}
+		} catch (err) {
+			console.error("Error:", err);
+		}
+	};
+
 	return (
 		<>
 			<div>Hello World</div>
+			<form onSubmit={handleSubmit}>
+				<label>URL: </label>
+				<input
+					type="text"
+					name="url"
+					value={url}
+					onChange={(evt) => setUrl(evt.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+			<div>Backend Response:</div>
+			<p>{text}</p>
 		</>
 	);
 }
